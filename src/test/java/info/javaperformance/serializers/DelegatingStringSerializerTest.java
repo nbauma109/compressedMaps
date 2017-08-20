4f5c7ae7e8ca273f7c03f1bcc6a5fb40a1b5502d
nbauma109 <nbauma109@github>
2017-08-20 21:09:06 +0200

    use less threads and mem in tests

--- src/test/java/info/javaperformance/serializers/DelegatingStringSerializerTest.java
+++ src/test/java/info/javaperformance/serializers/DelegatingStringSerializerTest.java
@@ -6,7 +6,7 @@ import junit.framework.TestCase;
 
 public class DelegatingStringSerializerTest extends TestCase {
 	public void testConcurrent() throws InterruptedException {
-		final int THREADS = 32;
+		final int THREADS = 3;
 		final DelegatingStringSerializer s = new DelegatingStringSerializer();
 		final CountDownLatch start = new CountDownLatch(THREADS);
 		final CountDownLatch end = new CountDownLatch(THREADS);
@@ -15,7 +15,7 @@ public class DelegatingStringSerializerTest extends TestCase {
 			final Runnable task = new Runnable() {
 				@Override
 				public void run() {
-					final ByteArray buf = new ByteArray().reset(new byte[10000000]);
+					final ByteArray buf = new ByteArray().reset(new byte[2400]);
 					start.countDown();
 					try {
 						start.await();
@@ -23,11 +23,11 @@ public class DelegatingStringSerializerTest extends TestCase {
 						e.printStackTrace();
 					}
 					buf.position(0);
-					for (int j = 0; j < 500000; ++j) {
+					for (int j = 0; j < 500; ++j) {
 						s.write(id + String.valueOf(j), buf);
 					}
 					buf.position(0);
-					for (int j = 0; j < 100000; ++j) {
+					for (int j = 0; j < 500; ++j) {
 						assertEquals(id + String.valueOf(j), s.read(buf));
 					}
 					end.countDown();
